7f9f276731141f2bf1e0dd155fe1d164cf22aaf7
lavendersonw <hottea4649@yahoo.co.jp>
2014-04-02 10:53:12 +0900

    Initial Commit

---
+++ MyCApp/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyCApp
+//
+//  Created by kanako on 2014/04/02.
+//  Copyright (c) 2014年 purple_rain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
